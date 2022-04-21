3f7cd59b559a7b959d5b484171ee32f2f51e2e73
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-04-21 19:53:21 +0530

    #include<stdio.h> int main() {     float r,area;     scanf("%f",&r);     area=3.14*r*r;     printf("%.2f",area);     return 0; }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -1,11 +1,10 @@
+
 #include<stdio.h>
 int main()
 {
-    int r;
-    float area;
-    scanf("%d",&r);
+    float r,area;
+    scanf("%f",&r);
     area=3.14*r*r;
     printf("%.2f",area);
     return 0;
-    
 }
