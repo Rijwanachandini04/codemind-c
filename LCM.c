c18b006827282f6786140ea4eec5df57b3c8c40b
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-04-21 20:38:57 +0530

    #include<stdio.h> int main() {     int m,n,max,lcm,i;     scanf("%d%d",&m,&n);     max=m>n?m:n;     lcm=max;     for(i=max;;i=i+max)     {         if(i%m==0&&i%n==0)         {             lcm=i;             break;         }     }     printf("%d",lcm); }

--- LCM.c
+++ LCM.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int m,n,max,lcm;
-    scanf("%d %d",&n,&m);
-    
+    int m,n,max,lcm,i;
+    scanf("%d%d",&m,&n);
     max=m>n?m:n;
     lcm=max;
-    for(int i=max;;i=i+max)
+    for(i=max;;i=i+max)
     {
-        if(i%m==0 && i%n==0)
+        if(i%m==0&&i%n==0)
         {
             lcm=i;
             break;
         }
     }
     printf("%d",lcm);
-    return 0;
 }
