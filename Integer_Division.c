5cf73edce42bc76b45fc936e42d1d73fbc8fb09a
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-04-21 20:18:31 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     long int x;     scanf("%ld",&x);     if(x>0)     {         printf("%ld",x/10);     }     else     {         if(abs(x)%10==0)         {             printf("%ld",x/10);         }         else         {             x=(abs(x)/10)+1;             printf("-%ld",x);         }     }
    }

---
+++ Integer_Division.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    long int x;
+    scanf("%ld",&x);
+    if(x>0)
+    {
+        printf("%ld",x/10);
+    }
+    else
+    {
+        if(abs(x)%10==0)
+        {
+            printf("%ld",x/10);
+        }
+        else
+        {
+            x=(abs(x)/10)+1;
+            printf("-%ld",x);
+        }
+    }
+
+    
+}
