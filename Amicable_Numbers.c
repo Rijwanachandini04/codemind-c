316b03a7bd06b005b264b74e1a4795b5a3747ec4
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-06-29 18:37:06 +0530

    #include<stdio.h> int main() {     int i,n1,n2,div1=0,div2=0;     scanf("%d%d",&n1,&n2);     for(i=1;i<n1;i++)     {         if(n1%i==0)         {             div1+=i;         }     }     for(i=1;i<n2;i++)     {         if(n2%i==0)         {             div2+=i;         }     }     if(n1==div2 && n2==div1)     {         printf("Amicable");     }else{         printf("Not Amicable");     }     return 0; }

--- Amicable_Numbers.c
+++ Amicable_Numbers.c
@@ -1,24 +1,28 @@
+
 #include<stdio.h>
 int main()
 {
-    int i,n1,n2,sum1=0,sum2=0;
+    int i,n1,n2,div1=0,div2=0;
     scanf("%d%d",&n1,&n2);
     for(i=1;i<n1;i++)
     {
         if(n1%i==0)
         {
-            sum1+=i;
+            div1+=i;
         }
     }
-    for(i=1;i<n1;i++)
+    for(i=1;i<n2;i++)
     {
         if(n2%i==0)
         {
-            sum2+=i;
+            div2+=i;
         }
     }
-    if(n1==sum2 && n2==sum1)
-    printf("Amicable");
-    else
-    printf("Not Amicable");
+    if(n1==div2 && n2==div1)
+    {
+        printf("Amicable");
+    }else{
+        printf("Not Amicable");
+    }
+    return 0;
 }
