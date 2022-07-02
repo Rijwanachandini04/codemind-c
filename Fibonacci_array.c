8a4618c589954379cc76bd79e299446e6e1c8c25
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-07-02 18:45:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int c=0;     for(int i=2;i<n;i++)     {         if(arr[i]==arr[i-1]+arr[i-2])         {             c=1;         }         else         {             c=0;             break;         }     }     if(c==1)     {         printf("yes");     }     else     {         printf("no");     } }

---
+++ Fibonacci_array.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int c=0;
+    for(int i=2;i<n;i++)
+    {
+        if(arr[i]==arr[i-1]+arr[i-2])
+        {
+            c=1;
+        }
+        else
+        {
+            c=0;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
